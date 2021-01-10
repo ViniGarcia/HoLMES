3def948c6951095cb5897d05a89ee879a64db565
Fulber-Garcia <vfulbergarcia@gmail.com.br>
2021-01-10 16:24:53 -0300

    Installing scripts; README updates

---
+++ install.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Please, manually install Python3, Python3-Pip, and SQLite3
+PAUSE
+ECHO Please, insert the "pip" command to the standard path
+PAUSE
+pip install flask
+pip install psutil
+pip install requests
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt-get install python3.9
+apt-get install python3-pip
+apt-get install sqlite3
+
+pip install flask
+pip install psutil
+pip install requests
+
+
